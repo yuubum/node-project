03ecae815a7d937fc52667b11d085ac160a3643a
SHUBHAM MAHARJAN <shubham.maharjan.2017@gmail.com>
2021-10-06 20:19:10 +0545

    Update index.js

--- index.js
+++ index.js
@@ -2,27 +2,7 @@ const http = require('http');
 const url = require('url');
 
 const server = http.createServer((request, response) => {
-    let content = "";
-    let statusCode = 200;
 
-    switch (request.url) {
-        case "/":
-            content = '<h1>This is root page.</h1>'
-            break;
-        case "/profile":
-            content = '<h1>This is profile page.</h1>'
-            break;
-        case "/about":
-            content = '<h1>This is about page</h1>'
-            break;
-        default:
-            content = '<h1>This is 404 page</h1>'
-            statusCode = 404;
-    }
-
-    response.writeHead(statusCode, { 'Content-Type': 'text/html' });
-    response.write(content);
-    response.end();
 });
 
 server.listen(3000, () => {
